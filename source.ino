6941a1d9f8d1d010cb18f4666e948f919193bc99
GreenSkittle <bpbraithwaite@gmail.com>
2020-07-17 15:17:16 -0700

    setters in place

--- source.ino
+++ source.ino
@@ -4,6 +4,8 @@
 #include "alcObjects.h"
 alckAdvanced myAlClock; // beautiful initialization.
 void setup() {
+    myAlClock.t_btSetter(true);
+    myAlClock.t_tTempSetter(true);
     myAlClock.alarmIsSet              = false;
     myAlClock.wakeTargetOffset.hour   = 10; // enter time in 24hr
     myAlClock.wakeTargetOffset.minute = 0;
