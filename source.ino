a87e3dd25aca672562f0281997eb788892094f35
GreenSkittle <bpbraithwaite@gmail.com>
2020-07-20 19:27:31 -0700

    ready to deploy

--- source.ino
+++ source.ino
@@ -2,14 +2,14 @@
     by Bradyn Braithwaite, 2020
 */
 #include "alcObjects.h"
-alckAdvanced myAlClock; // beautiful initialization.
+alckAdvanced nanoAlck;
 void setup() {
-    myAlClock.t_btSetter(true);
-    myAlClock.t_tTempSetter(true);
-    myAlClock.alarmIsSet              = false;
-    myAlClock.wakeTargetOffset.hour   = 10; // enter time in 24hr
-    myAlClock.wakeTargetOffset.minute = 0;
+    nanoAlck.dynamicBrightness = true;
+    nanoAlck.useTempRoutine    = true;
+    nanoAlck.alarmIsSet        = false;
+    nanoAlck.wakeTargetOffset.setHour(10);
+    nanoAlck.wakeTargetOffset.setMinute(0x0);
 }
 void loop() {
-    myAlClock.runNow(); // it loops
+    nanoAlck.runNow();
 }
