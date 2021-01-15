0844cff3a699300e1e87c5a26daf51b0f0f527cf
bradynp <bpbraithwaite@gmail.com>
2021-01-14 22:10:04 -0700

    customize pin mapping

--- src/src.ino
+++ src/src.ino
@@ -1,13 +1,21 @@
 #include "alckObjects.h"
-#include "alckutils.h"
-alck arduinoAlck;
+
+#define ATTINY 1
+// board type used to determine pin mappings
+// Order: displayClockPin displayDataIOPin button_A_setter button_B_hour
+// button_C_minute buzzerPin
+#ifdef SEEDUINO
+uint8_t pinMap[6] = {2, 3, 5, 4, 8, 9};
+#endif
+#ifdef ATTINY
+uint8_t pinMap[6] = {0, 1, 2, 3, 4, 5};
+#endif
+alck arduinoAlck(pinMap);
 void setup() {
-    //stringToAlarmTime(&arduinoAlck, "8:00 AM");
-    arduinoAlck.wakeTargetOffset.setHour(8);
-    arduinoAlck.wakeTargetOffset.setMinute(0);
-    arduinoAlck.alarmIsSet        = false;
-    arduinoAlck.brightnessRoutine = true;
+  // stringToAlarmTime(&arduinoAlck, "8:00 AM");
+  arduinoAlck.wakeTargetOffset.setHour(8);
+  arduinoAlck.wakeTargetOffset.setMinute(0);
+  arduinoAlck.alarmIsSet = false;
+  arduinoAlck.brightnessRoutine = true;
 }
-void loop() {
-    arduinoAlck.execute();
-}
+void loop() { arduinoAlck.execute(); }
